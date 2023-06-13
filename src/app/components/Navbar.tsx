836478adfbd7b571f00247c0b540602ffa155203
Pavnuto <chuvachek@list.ru>
2023-06-13 22:06:55 +0300

    Add tailwind

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav>
+<ul className="flex items-center justify-center gap-2">
+        <li className="font-bold">simple blog - vini.bortoletto</li>
+    <li>
+    </li>
+</ul>
+    </nav>
+  )
+}
